58848ef32f0084cb8afc5640784a0663bbbeca25
RenatoCernades0107 <renatocernades0107@gmail.com>
2024-10-19 01:26:33 -0500

    normalization

--- EmbedingProducts.py
+++ EmbedingProducts.py
@@ -5,8 +5,16 @@ def create_product_vector():
     """Creates a vector with the sum of likes and shares for each product"""
     products = get_all_products()
     product_vector = []
+    maximum = -1
     for product in products:
         likes = get_product_likes(product)
         shares = get_product_shares(product)
         product_vector.append(likes + shares)
+
+        if likes + shares > maximum:
+            maximum = likes + shares
+    
+    for i in range(len(product_vector)):
+        product_vector[i] /= maximum
+        
     return product_vector
--- Knn.py
+++ Knn.py
@@ -1,9 +1,11 @@
 # TODO: TOP k products with the highest values in the vector
 from EmbedingProducts import create_product_vector, get_all_products
+from SkinAdvisorFeatures import get_skin_advisor_features
 
 def top_k_products(k):
     """Returns the top k products with the highest values in the vector"""
     product_vector = create_product_vector()
+    product_vector += get_skin_advisor_features()
     sorted_products = sorted(zip(product_vector, get_all_products()), reverse=True)
     top_k = [product for _, product in sorted_products[:k]]
     return top_k
